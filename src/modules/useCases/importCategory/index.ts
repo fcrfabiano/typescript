4ad4075cdd789ffa448c60c5a0edca6684c94f26
Fabiano César <fcfabiano.cesar@icloud.com>
2021-03-18 21:16:27 -0300

    Fim do projeto

---
+++ src/modules/useCases/importCategory/index.ts
@@ -0,0 +1,11 @@
+import { CategoriesRepository } from "../../cars/repositories/implementations/CategoriesResitory";
+import { ImportCategoryController } from "./ImportCategoryController";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase
+);
+
+export { importCategoryController };
