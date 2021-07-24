dbeae91dde0731e239b909decff8f55b61d47c0a
Fabiano <fcfabiano.cesar@icloud.com>
2021-07-24 19:27:25 -0300

    :tada:

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecifications/CreateSpecificationController";
+
+const specificationsRoutes = Router();
+
+const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.post("/", createSpecificationController.handle);
+
+export { specificationsRoutes };
