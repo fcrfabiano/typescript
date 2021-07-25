4b249a61375cdc64ef5410899a968c82e846ce41
Fabiano <fcfabiano.cesar@icloud.com>
2021-07-25 18:30:23 -0300

    Upload de avatar adicionado

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { deleteFile } from "../../../../utils/file";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  id: string;
+  avatar: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({ id, avatar }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
+
+    user.avatar = avatar;
+
+    await this.usersRepository.create(user);
+  }
+}
+
+export { UpdateUserAvatarUseCase };
